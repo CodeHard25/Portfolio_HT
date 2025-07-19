04675e1e2e103336ea516ef70fd16d18e2294169
CodeHard25 <hardiktyagi007@gmail.com>
2025-07-19 15:11:39 +0530

    resume update

--- src/Components/AboutSection.jsx
+++ src/Components/AboutSection.jsx
@@ -35,7 +35,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="https://drive.google.com/file/d/1SpUaFrSFwyaoP8RyZeIV2UGoHtbshGHs/view?usp=drive_link"
+                href="https://drive.google.com/file/d/19wn7uMyKMK-d3GCG85iwnZcnNdXNe6W9/view?usp=drive_link"
                 className="px-6 py-2 rounded-full bg-primary text-amber-50  border border-primary  hover:bg-primary/5 hover:text-blue-900 transition-colors duration-300"
                download>
                 Download CV
